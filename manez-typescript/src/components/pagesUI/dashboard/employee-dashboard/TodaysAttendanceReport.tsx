61d9d2aaacb1b6e17a566040d2d14be0af680891
tejas0901 <tejasnigam09@gmail.com>
2025-12-17 18:45:43 +0530

    fixed error in todayAttendanceReport

--- manez-typescript/src/components/pagesUI/dashboard/employee-dashboard/TodaysAttendanceReport.tsx
+++ manez-typescript/src/components/pagesUI/dashboard/employee-dashboard/TodaysAttendanceReport.tsx
@@ -5,7 +5,7 @@ const TodaysAttendanceReport = () => {
   return (
     <div className="card__wrapper">
       <div className="card__title-wrap mb-4">
-        <h5 className="card__heading-title">Today's Attendance</h5>
+        <h5 className="card__heading-title">Todays Attendance</h5>
       </div>
       <div className="flex justify-center items-center h-48">
         <p>Attendance data not available</p>
